2d41793d355c7ae2c77c69f217a79fed11dd61a6
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-07-05 00:57:17 +0200

    from in rxJS

--- of.js
+++ of.js
@@ -1,4 +1,11 @@
-import { of, range } from "rxjs"
+var { of, range, from } = rxjs;
+
+function* hello() { //utworzenie funkcji będącej iteratorem
+    yield 'Hello';
+    yield ' World';
+}
+
+const iterator = hello();
 
 const observer = {
     next: val => console.log('next', val),
@@ -8,6 +15,10 @@ const observer = {
 
 const source$ = of(1,2,3,4,5);
 const source2$ = range(1,5);
+const sourceFrom$ = from('Hello');
+const sourceIterator$ = from(iterator);
 
 // source$.subscribe(observer);
-source2$.subscribe(observer);
+// sourceFrom$.subscribe(observer);
+sourceIterator$.subscribe(observer);
+
