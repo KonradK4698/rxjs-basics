a2fc5d264b564c9b35619f2c0b263210666714e4
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-07-05 23:19:09 +0200

    tap operator

---
+++ tap.js
@@ -0,0 +1,13 @@
+import { of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+const numbers$ = of(1,2,3,4,5); 
+
+numbers$.pipe(
+    tap(value => console.log(`before: ${value}`)),
+    map(value => value * 10),
+    tap({
+        next: value => console.log(`after: ${value}`),
+        complete: () => console.log('tap complete')
+    }),
+).subscribe(value => console.log(`from subscribe: ${value}`))
