6029f58ea8ba8317688ae540a98f6aee0140bee6
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-07-05 21:45:07 +0200

    filter operator

---
+++ filter.js
@@ -0,0 +1,19 @@
+const { of, fromEvent } = rxjs; 
+const { filter, map } = rxjs.operators;
+
+of(1,2,3,4,5).pipe(
+    filter(value => value > 2)
+).subscribe(console.log)
+
+const keyup$ = fromEvent(document, 'keyup'); 
+
+const keycode$ = keyup$.pipe(
+    map(event => event.code)
+);
+
+const enter$ = keycode$.pipe(
+    filter(code => code === "Enter")
+);
+
+enter$.subscribe(console.log);
+keycode$.subscribe(console.log)
