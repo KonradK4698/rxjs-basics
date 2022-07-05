f8cb42241fcdf149f5bfcf47d53272a1203d9776
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-07-05 23:07:54 +0200

    countdown timer

---
+++ countDownTimer.js
@@ -0,0 +1,20 @@
+const { interval } = rxjs;
+const { scan, mapTo, filter } = rxjs.operators;
+
+const countdown = document.querySelector('#countDown')
+const message = document.querySelector('#message')
+
+const counter$ = interval(1000);
+
+counter$.pipe( 
+    mapTo(-1), //odpowiada za zmniejszanie wartości
+    scan((accumulator, current) => {
+        return accumulator + current;
+    }, 10),
+    filter(value => value >= 0)
+).subscribe(value => {
+    countdown.innerHTML = value;
+    if(!value){
+        message.innerHTML = 'Liftoff!';
+    }
+})
